06a176b6b99da576320dbe4fde83468aa7c8852e
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-03-18 16:33:32 +0100

    better description

--- make-marmalade-pkg.sh
+++ make-marmalade-pkg.sh
@@ -5,7 +5,7 @@ VERSION=$(grep Version: emstar.el | sed 's|^;;[ ]*Version:[ ]*\(.*\)$|\1|g')
 mkdir -p emstar-$VERSION/
 cat <<EOF > emstar-$VERSION/emstar-pkg.el
 (define-package "emstar" "$VERSION"
-                 "Play Emstar in emacs.")
+                "Casual game, like a brainy Pac-Man")
 EOF
 
 cp -R emstar-levels  emstar.el emstar-$VERSION/
